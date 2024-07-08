b387b38e764ea884ae636109e2f2d181b2778684
manoharmeena12345 <manoharmeena245@gmail.com>
2024-07-08 15:27:30 +0530

    use auth fixed

--- src/app/single-discussion/page.tsx
+++ src/app/single-discussion/page.tsx
@@ -1,14 +1,17 @@
 // app/single-discussion/page.tsx
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
-
-const SingleDiscussion = dynamic(() => import('@/markup/Element/SingleDiscussion'), {
-  ssr: false,
-});
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
+const SingleDiscussion = dynamic(
+  () => import("@/markup/Element/SingleDiscussion"),
+  {
+    ssr: false,
+  }
+);
 
 const SingleDiscussionPage = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <SingleDiscussion />
     </Suspense>
   );
