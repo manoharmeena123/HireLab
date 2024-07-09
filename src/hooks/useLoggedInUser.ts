5a7cf61bbdaa9111ee101876650762c0e36b15ab
manoharmeena12345 <manoharmeena245@gmail.com>
2024-07-09 14:25:48 +0530

    post job done

--- src/hooks/useLoggedInUser.ts
+++ src/hooks/useLoggedInUser.ts
@@ -5,7 +5,6 @@ import { useGetLoggedInUserQuery } from "@/app/login/store/login.query";
 export const useLoggedInUser = () => {
   const { data, error, isLoading, refetch } = useGetLoggedInUserQuery();
 
-  console.log("useLoggedInUser", data);
   return {
     user: data,
     error,
