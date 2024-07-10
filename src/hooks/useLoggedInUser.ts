2d12cc459e600d2b7d4b453d6261bdfd198a1e15
manoharmeena12345 <manoharmeena245@gmail.com>
2024-07-10 15:03:16 +0530

    logout fixed

--- src/hooks/useLoggedInUser.ts
+++ src/hooks/useLoggedInUser.ts
@@ -1,15 +1,42 @@
-"use client"
+"use client";
 // src/hooks/useLoggedInUser.ts
+import { useState, useEffect } from 'react';
 import { useGetLoggedInUserQuery } from "@/app/login/store/login.query";
 
 export const useLoggedInUser = () => {
   const { data, error, isLoading, refetch } = useGetLoggedInUserQuery();
+  const [user, setUser] = useState<any | null>(null);
+  const [fetchError, setFetchError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (isLoading) {
+      setLoading(true);
+    } else if (error) {
+      setLoading(false);
+      setFetchError(error as Error);
+    } else if (data) {
+      setLoading(false);
+      setUser(data);
+    }
+  }, [data, error, isLoading]);
+
+  const refetchUser = async () => {
+    setLoading(true);
+    setFetchError(null);
+    try {
+      await refetch();
+    } catch (error) {
+      setFetchError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return {
-    user: data,
-    error,
-    isLoading,
-    refetch
+    user,
+    error: fetchError,
+    isLoading: loading,
+    refetch: refetchUser
   };
 };
-
