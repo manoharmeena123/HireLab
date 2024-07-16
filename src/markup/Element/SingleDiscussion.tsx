12e634ee9ec62cbbf0761bacdeb648351a43ed3d
Muhammed Najeeb <muhdnajeeb3@gmail.com>
2024-07-17 04:49:52 +0530

    single discussion page done

--- src/markup/Element/SingleDiscussion.tsx
+++ src/markup/Element/SingleDiscussion.tsx
@@ -7,6 +7,10 @@ import Link from "next/link";
 import bnr from "../../images/banner/bnr1.jpg";
 import { useGetSingleDiscussionByTitleMutation } from "@/store/global-store/global.query";
 import Loading from "@/components/Loading";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CommentSection } from "react-comments-section";
+import "react-comments-section/dist/index.css";
 
 const SingleDiscussion = () => {
   const searchParams = useSearchParams();
@@ -16,7 +20,17 @@ const SingleDiscussion = () => {
     getSingleDiscussionByTitle,
     { data: singleDiscussion, isLoading, isError, isSuccess, error },
   ] = useGetSingleDiscussionByTitleMutation();
-
+  const data = [
+    {
+      userId: "02b",
+      comId: "017",
+      fullName: "Lily",
+      userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
+      text: "I think you have a point🤔",
+      avatarUrl: "https://ui-avatars.com/api/name=Lily&background=random",
+      replies: [],
+    },
+  ];
   useEffect(() => {
     if (query) {
       getSingleDiscussionByTitle(query);
@@ -53,45 +67,142 @@ const SingleDiscussion = () => {
           <div className="content-area">
             <div className="container">
               <div className="row">
-                <div className="col-lg-8 col-md-7 m-b10">
+                <div className="col-lg-9 col-md-8 m-b10">
+                  <h2>MBA Dual Specialization at GJIMT Mohali</h2>
                   <div className="blog-post blog-single blog-style-1">
-                    <div className="dez-post-meta">
-                      <ul className="d-flex align-items-center">
-                        <li className="post-date">
-                          <i className="fa fa-calendar"></i>
-                          {created_at}
-                        </li>
-                        <li className="post-author">
-                          <i className="fa fa-user"></i>
-                          {user?.name}
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="dez-post-title">
-                      <h4 className="post-title m-t0">{question}</h4>
-                    </div>
-                    <div className="dez-post-media dez-img-effect zoom-slow m-t20">
-                      <Image
-                        src={bnr.src}
-                        alt={question}
-                        width={600}
-                        height={400}
-                      />
-                    </div>
-                    <div className="dez-post-text">
-                      <p>{description}</p>
-                    </div>
-                    <div className="d-flex justify-content-between align-items-center mt-3">
-                      <div className="d-flex align-items-center">
-                        <span className="mr-3">❤️ {likes}</span>
-                        <span className="mr-3">💬 {comments}</span>
-                        <span>👀 {views}</span>
-                      </div>
-                    </div>
+                    <CommentSection
+                      currentUser={{
+                        currentUserId: "01a",
+                        currentUserImg:
+                          "https://ui-avatars.com/api/name=Riya&background=random",
+                        currentUserProfile:
+                          "https://www.linkedin.com/in/riya-negi-8879631a9/",
+                        currentUserFullName: "Riya Negi",
+                      }}
+                      logIn={{
+                        loginLink: "http://localhost:3001/",
+                        signupLink: "http://localhost:3001/",
+                      }}
+                      commentData={data}
+                      onSubmitAction={(data: {
+                        userId: string;
+                        comId: string;
+                        avatarUrl: string;
+                        userProfile?: string;
+                        fullName: string;
+                        text: string;
+                        replies: any;
+                        commentId: string;
+                      }) => console.log("check submit, ", data)}
+                      currentData={(data: any) => {
+                        console.log("curent data", data);
+                      }}
+                    />
                   </div>
                 </div>
-                <div className="col-lg-4 col-md-5 sticky-top">
-                  <Sidebar />
+                <div className="col-lg-3 col-md-4 sticky-top">
+                  <div className="suggested-topics-wrap shadow p-3">
+                    <div>
+                      <h5>Suggested Topics</h5>
+                      <hr />
+                    </div>
+                    <div>
+                      <div className="sugg-comment-topic">
+                        <h6>How To Study in the Top College</h6>
+                        <div className="sugg-topic-card-detail">
+                          <div className="d-flex disc-date-suggest">
+                            <div className="">
+                              <span className="badge-category-bg"></span>
+                              <span>MBA</span>
+                            </div>
+                            <div>
+                              <span>Oct '23</span>
+                            </div>
+                          </div>
+                          <div className="disc-date-suggest">
+                            <span>12</span>
+                            <FontAwesomeIcon icon={faComment} />
+                          </div>
+                        </div>
+                      </div>
+                      <hr />
+                      <div className="sugg-comment-topic">
+                        <h6>How To Study in the Top College</h6>
+                        <div className="sugg-topic-card-detail">
+                          <div className="d-flex disc-date-suggest">
+                            <div className="">
+                              <span className="badge-category-bg"></span>
+                              <span>MBA</span>
+                            </div>
+                            <div>
+                              <span>Oct '23</span>
+                            </div>
+                          </div>
+                          <div className="disc-date-suggest">
+                            <span>12</span>
+                            <FontAwesomeIcon icon={faComment} />
+                          </div>
+                        </div>
+                      </div>
+                      <hr />
+                      <div className="sugg-comment-topic">
+                        <h6>How To Study in the Top College</h6>
+                        <div className="sugg-topic-card-detail">
+                          <div className="d-flex disc-date-suggest">
+                            <div className="">
+                              <span className="badge-category-bg"></span>
+                              <span>MBA</span>
+                            </div>
+                            <div>
+                              <span>Oct '23</span>
+                            </div>
+                          </div>
+                          <div className="disc-date-suggest">
+                            <span>12</span>
+                            <FontAwesomeIcon icon={faComment} />
+                          </div>
+                        </div>
+                      </div>
+                      <hr />
+                      <div className="sugg-comment-topic">
+                        <h6>How To Study in the Top College</h6>
+                        <div className="sugg-topic-card-detail">
+                          <div className="d-flex disc-date-suggest">
+                            <div className="">
+                              <span className="badge-category-bg"></span>
+                              <span>MBA</span>
+                            </div>
+                            <div>
+                              <span>Oct '23</span>
+                            </div>
+                          </div>
+                          <div className="disc-date-suggest">
+                            <span>12</span>
+                            <FontAwesomeIcon icon={faComment} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="my-5">
+                      <p className="suggested-topics-message">
+                        <b>Want to read more? Browse other topics in </b>
+                        <Link
+                          className="badge-wrapper bullet d-inline-block"
+                          href="/c/mba/373"
+                        >
+                          <div className="">
+                            <span className="badge-category-bg"></span>
+                            <span>MBA</span>
+                          </div>
+                        </Link>{" "}
+                        or{" "}
+                        <Link href="/latest" style={{ color: "blue" }}>
+                          view latest topics
+                        </Link>
+                        .
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
