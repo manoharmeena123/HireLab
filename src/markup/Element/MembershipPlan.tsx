ca88d4e405ce0076ad601246c639a4aabf14db2a
manoharmeena12345 <manoharmeena245@gmail.com>
2024-07-25 18:19:02 +0530

    membership fixed

--- src/markup/Element/MembershipPlan.tsx
+++ src/markup/Element/MembershipPlan.tsx
@@ -6,13 +6,18 @@ const bnr7 = require("./../../images/background/plans.png");
 
 const MembershipPlans = () => {
   const { data: membershipData } = useGetMembershipQuery();
+
+  const parseHtml = (htmlString: any) => {
+    return { __html: htmlString };
+  };
+
   return (
     <div className="section-content box-sort-in button-example m-t80">
       <div className="pricingtable-row">
         <div className="display-property">
           {membershipData?.data?.map((text, index) => (
             <div
-            key={index}
+              key={index}
               className="pricingtable-wrapper style2 bg-white member-ship-div"
               style={{
                 minHeight: "500px",
@@ -25,6 +30,16 @@ const MembershipPlans = () => {
                 ) as HTMLElement;
                 button.style.backgroundColor = "#fff";
                 button.style.color = "#000";
+
+                const title = e.currentTarget.querySelector(
+                  ".pricingtable-title"
+                ) as HTMLElement;
+                title.style.color = "#000";
+
+                const description = e.currentTarget.querySelector(
+                  ".pricingtable-description"
+                ) as HTMLElement;
+                description.style.color = "#fff";
               }}
               onMouseLeave={(e) => {
                 const button = e.currentTarget.querySelector(
@@ -32,12 +47,22 @@ const MembershipPlans = () => {
                 ) as HTMLElement;
                 button.style.backgroundColor = "#2A6310";
                 button.style.color = "#fff";
+
+                const title = e.currentTarget.querySelector(
+                  ".pricingtable-title"
+                ) as HTMLElement;
+                title.style.color = "";
+
+                const description = e.currentTarget.querySelector(
+                  ".pricingtable-description"
+                ) as HTMLElement;
+                description.style.color = "";
               }}
             >
-              <div className="">
+              <div className="pricingtable-inner">
                 <div className="pricingtable-price">
                   <h4
-                    className="font-weight-900 m-t10 m-b0 text-center"
+                    className="font-weight-900 m-t10 m-b0 text-center pricingtable-title"
                     style={{
                       fontWeight: "bold",
                       fontSize: "28px",
@@ -48,11 +73,14 @@ const MembershipPlans = () => {
                   >
                     {text?.title}
                   </h4>
-                  <p className="text-center my-2">{text?.description}</p>
+                  <p
+                    className="text-center my-2 pricingtable-description"
+                    dangerouslySetInnerHTML={parseHtml(text?.description)}
+                  ></p>
                 </div>
                 <div className="m-t10 price-wrap">
                   <h3
-                    className="font-weight-300 m-t10 m-b0"
+                    className="font-weight-300 m-t10 m-b0 price-title"
                     style={{
                       fontSize: "20px",
                       textAlign: "center",
@@ -61,35 +89,37 @@ const MembershipPlans = () => {
                   >
                     Price
                   </h3>
-                  <ul className="mb-0">
+                  <ul className="mb-0 price-list">
                     <li
+                      className="price-item"
                       style={{
                         color: "black",
                         margin: "5px 0px",
                         fontSize: "18px",
                       }}
                     >
-                      <b>{text?.price}/-</b> for 1 month
+                      <b>{text?.monthly_price}</b>
                     </li>
                     <li
+                      className="price-item"
                       style={{
                         color: "black",
                         margin: "5px 0px",
                         fontSize: "18px",
                       }}
                     >
-                      <del className="text-red">{text?.discount_price}/-</del>{" "}
-                      <b>1799/-</b> for 3 months
+                      <del className="text-red">{text?.quarterly_price}</del>
                     </li>
                   </ul>
                 </div>
                 {text.info?.map((e, i) => (
                   <ul
-                    style={{ marginTop: "20px" }}
                     key={i}
                     className="mp-cards"
+                    style={{ marginTop: "20px" }}
                   >
                     <li
+                      className="mp-card-item"
                       style={{
                         color: "black",
                         margin: "0px 0px",
@@ -101,7 +131,7 @@ const MembershipPlans = () => {
                   </ul>
                 ))}
 
-                <div className="text-center">
+                <div className="text-center button-wrap">
                   <Link
                     href={"/register"}
                     className="site-button radius-xl white-hover"
@@ -111,7 +141,7 @@ const MembershipPlans = () => {
                     }}
                   >
                     <span
-                      className="p-lr30"
+                      className="p-lr30 button-text"
                       style={{ fontFamily: "__Inter_Fallback_aaf875" }}
                     >
                       Get Started
@@ -127,41 +157,91 @@ const MembershipPlans = () => {
         .member-ship-div {
           min-height: 500px;
           border-radius: 20px;
+          padding: 20px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          transition: background-color 0.3s, color 0.3s;
+          margin: 20px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
         }
         .member-ship-div:hover {
           background-color: #2a6310 !important;
           color: white !important;
         }
         .member-ship-div:hover .site-button {
-          background-color: #fff !important; /* Changes the button background color to white */
-          color: #000 !important; /* Changes the button text color to black */
+          background-color: #fff !important;
+          color: #000 !important;
         }
-        .member-ship-div:hover div h4 {
+        .member-ship-div:hover .pricingtable-title {
+          color: #000 !important;
+        }
+        .member-ship-div:hover .pricingtable-description {
+          color: #fff !important;
+        }
+        .member-ship-div:hover .price-title,
+        .member-ship-div:hover .price-item,
+        .member-ship-div:hover .mp-card-item {
           color: white !important;
-          border-color: white !important;
         }
-        .member-ship-div:hover div ul li {
-          color: white !important;
+        .pricingtable-wrapper.style2 .pricingtable-price {
+          padding: 10px;
         }
-        .white-hover {
-          border: 1px solid white !important;
+        .pricingtable-inner {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          height: 100%;
         }
-        .member-ship-div:hover div div h3 {
-          color: white !important;
+        .pricingtable-title {
+          font-weight: bold;
+          font-size: 28px;
+          border: 1.5px solid black;
+          border-radius: 20px;
+          padding: 5px;
+          text-align: center;
+          margin-bottom: 15px;
+        }
+        .pricingtable-description {
+          text-align: center;
+          margin-bottom: 20px;
+        }
+        .price-wrap {
+          text-align: center;
+          margin-bottom: 20px;
+        }
+        .price-title {
+          font-size: 20px;
+          font-weight: 700;
+        }
+        .price-list {
+          list-style: none;
+          padding: 0;
+        }
+        .price-item {
+          color: black;
+          margin: 5px 0;
+          font-size: 18px;
+        }
+        .button-wrap {
+          text-align: center;
+          margin-top: auto;
         }
         .display-property {
           display: grid;
-          grid-template-columns: repeat(4, 1fr);
+          grid-template-columns: repeat(3, 1fr);
           gap: 20px;
         }
-        .pagination-bx .pagination {
-          width: 300px;
-          margin-left: 0px;
+        .site-button {
+          border: 1px solid white;
+          background-color: #2a6310;
+          padding: 10px 30px;
+          border-radius: 30px;
+          color: white;
+          transition: background-color 0.3s, color 0.3s;
         }
-        .display-item-center {
-          display: flex !important;
-          align-item: center;
-          justify-content: center;
+        .button-text {
+          font-family: "__Inter_Fallback_aaf875";
         }
         @media (max-width: 1200px) {
           .display-property {
@@ -170,13 +250,13 @@ const MembershipPlans = () => {
         }
         @media (max-width: 576px) {
           .display-property {
-            grid-template-columns: repeat(4, 1fr);
-            overflow-x:scroll;
+            grid-template-columns: 1fr;
+            overflow-x: scroll;
             padding-left: 1rem;
           }
-          .member-ship-div{
-            min-width:350px;
-            min-height:auto;
+          .member-ship-div {
+            min-width: 350px;
+            min-height: auto;
           }
         }
       `}</style>
