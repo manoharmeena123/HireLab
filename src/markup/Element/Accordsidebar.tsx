038bb2ec709681f2e2249ac9714f4bb4e2367d8f
Muhammed Najeeb <muhdnajeeb3@gmail.com>
2024-08-01 11:53:46 +0530

    ctc range implementation

--- src/markup/Element/Accordsidebar.tsx
+++ src/markup/Element/Accordsidebar.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { Accordion } from "react-bootstrap";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
 
 interface Filters {
   experience: string[];
@@ -28,11 +30,11 @@ const Accordsidebar: React.FC<AccordsidebarProps> = ({
     setFilters({ ...filters, [type]: newValues });
   };
   const [activeIndex, setActiveIndex] = useState(null);
+  const [value, setValue] = useState([30, 60]);
 
-  const handleClick = (index :any) => {
+  const handleClick = (index: any) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
-
 
   const resetFilters = () => {
     setFilters({
@@ -68,209 +70,18 @@ const Accordsidebar: React.FC<AccordsidebarProps> = ({
         </h6>
         <ul className="accordion-list">
           <li
-            className={`accordion-item ${activeIndex === 0 ? "active" : ""}`}
-            onClick={() => handleClick(0)}
+            className={`accordion-item ${activeIndex === 5 ? "active" : ""}`}
+            onClick={() => handleClick(5)}
           >
-            <h3>Experience</h3>
-            <div className={`answer ${activeIndex === 0 ? "show" : ""}`}>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="fresher"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("experience", "Fresher Only")
-                  }
-                  checked={filters?.experience?.includes("Fresher Only")}
-                />
-                <label className="custom-control-label" htmlFor="fresher">
-                  Fresher Only
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="experience"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("experience", "Experience Only")
-                  }
-                  checked={filters?.experience?.includes("Experience Only")}
-                />
-                <label className="custom-control-label" htmlFor="experience">
-                  Experience Only
-                </label>
-              </div>
-            </div>
-          </li>
-
-          <li
-            className={`accordion-item ${activeIndex === 1 ? "active" : ""}`}
-            onClick={() => handleClick(1)}
-          >
-            <h3>Location</h3>
-            <div className={`answer ${activeIndex === 1 ? "show" : ""}`}>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="workFromHome"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("location", "Work From Home")
-                  }
-                  checked={filters?.location?.includes("Work From Home")}
-                />
-                <label className="custom-control-label" htmlFor="workFromHome">
-                  Work From Home
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="workFromOffice"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("location", "Work From Office")
-                  }
-                  checked={filters?.location?.includes("Work From Office")}
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="workFromOffice"
-                >
-                  Work From Office
-                </label>
-              </div>
-            </div>
-          </li>
-
-          <li
-            className={`accordion-item ${activeIndex === 2 ? "active" : ""}`}
-            onClick={() => handleClick(2)}
-          >
-            <h3>Education</h3>
-            <div className={`answer ${activeIndex === 2 ? "show" : ""}`}>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="education10th"
-                  type="checkbox"
-                  onChange={() => handleFilterChange("education", "10th")}
-                  checked={filters?.education?.includes("10th")}
-                />
-                <label className="custom-control-label" htmlFor="education10th">
-                  10th
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="education12th"
-                  type="checkbox"
-                  onChange={() => handleFilterChange("education", "12th")}
-                  checked={filters?.education?.includes("12th")}
-                />
-                <label className="custom-control-label" htmlFor="education12th">
-                  12th
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="educationGraduate"
-                  type="checkbox"
-                  onChange={() => handleFilterChange("education", "Graduate")}
-                  checked={filters?.education?.includes("Graduate")}
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="educationGraduate"
-                >
-                  Graduate
-                </label>
-              </div>
-            </div>
-          </li>
-
-          <li
-            className={`accordion-item ${activeIndex === 3 ? "active" : ""}`}
-            onClick={() => handleClick(3)}
-          >
-            <h3>Cities</h3>
-            <div className={`answer ${activeIndex === 3 ? "show" : ""}`}>
-              {cities.map((city) => (
-                <div className="custom-control custom-checkbox" key={city}>
-                  <input
-                    className="custom-control-input"
-                    id={city}
-                    type="checkbox"
-                    onChange={() => handleFilterChange("cities", city)}
-                    checked={filters?.cities?.includes(city)}
-                  />
-                  <label className="custom-control-label" htmlFor={city}>
-                    {city}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </li>
-
-          <li
-            className={`accordion-item ${activeIndex === 4 ? "active" : ""}`}
-            onClick={() => handleClick(4)}
-          >
-            <h3>Job Titles</h3>
-            <div className={`answer ${activeIndex === 4 ? "show" : ""}`}>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="nodejsDeveloper"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("jobTitles", "Nodejs Developer")
-                  }
-                  checked={filters?.jobTitles?.includes("Nodejs Developer")}
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="nodejsDeveloper"
-                >
-                  Nodejs Developer
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="reactDeveloper"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("jobTitles", "React Developer")
-                  }
-                  checked={filters?.jobTitles?.includes("React Developer")}
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="reactDeveloper"
-                >
-                  React Developer
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id="laravelDeveloper"
-                  type="checkbox"
-                  onChange={() =>
-                    handleFilterChange("jobTitles", "Laravel Developer")
-                  }
-                  checked={filters?.jobTitles?.includes("Laravel Developer")}
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor="laravelDeveloper"
-                >
-                  Laravel Developer
-                </label>
+            <h3>CTC Based</h3>
+            <div
+              className={`answer ${activeIndex === 5 ? "show" : ""} pt-4`}
+              style={{ minHeight: "100px" }}
+            >
+              <RangeSlider value={value} onInput={setValue} />
+              <div className="d-flex justify-content-between gap-1 mt-2">
+                <span>{value[0]}</span>
+                <span>{value[1]}</span>
               </div>
             </div>
           </li>
