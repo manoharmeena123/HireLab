01732dc30c899e4936ddd7a04fc81c4539d187b0
manoharmeena12345 <manoharmeena245@gmail.com>
2024-09-11 13:40:31 +0530

    firebase

--- src/app/chats/lib/firebase.js
+++ src/app/chats/lib/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAQXpDYQD57UtkEKvIPKAaH4zHgOSHVI6A",
-  authDomain: "hirelab-chat.firebaseapp.com",
-  projectId: "hirelab-chat",
-  storageBucket: "hirelab-chat.appspot.com",
-  messagingSenderId: "849696165470",
-  appId: "1:849696165470:web:d1578e91ba9040f36baecf",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAQXpDYQD57UtkEKvIPKAaH4zHgOSHVI6A",
+//   authDomain: "hirelab-chat.firebaseapp.com",
+//   projectId: "hirelab-chat",
+//   storageBucket: "hirelab-chat.appspot.com",
+//   messagingSenderId: "849696165470",
+//   appId: "1:849696165470:web:d1578e91ba9040f36baecf",
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage()
+// export const auth = getAuth()
+// export const db = getFirestore()
+// // export const storage = getStorage()
